2aaeb6ae660e627bc35c9571ebbfd881b02de074
Obeeron <obeeron@hotmail.com>
2019-06-24 16:37:52 +0200

    Obeeron'sFirstCommit

--- ClientFTP/FTP/Connection.cs
+++ ClientFTP/FTP/Connection.cs
@@ -11,11 +11,11 @@ namespace ClientFTP
         public static void Start()
         {
             // create an FTP client
-            client = new FtpClient("192.168.1.16");
+            client = new FtpClient("127.0.0.1");
             client.Port = 21;
 
             // if you don't specify login credentials, we use the "anonymous" user account
-            client.Credentials = new NetworkCredential("User", "1234");
+            client.Credentials = new NetworkCredential("admin", "admin1234");
 
             // begin connecting to the server
             client.Connect();
